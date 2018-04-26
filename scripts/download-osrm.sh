bdc31a4758976b59d7c269241c9eb5cd95b7fca9
Agustín Rodríguez <agurz@icloud.com>
2018-04-25 23:51:23 -0300

    extract to data/osrm?

--- scripts/download-osrm.sh
+++ scripts/download-osrm.sh
@@ -1,6 +1,8 @@
 if [ ! -d "data/osrm/argentina" ]
 then 
     wget https://s3.amazonaws.com/run-forrest/argentina.tar.gz
-    mkdir -p data/osrm/argentina
-    tar zxvf argentina.tar.gz -C data/osrm/argentina
+    pwd
+    ls
+    mkdir -p data/osrm
+    tar zxvf argentina.tar.gz -C data/osrm
 fi
