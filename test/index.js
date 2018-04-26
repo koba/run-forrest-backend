7228b065d38ae5f2c955b65874d5cc9705c6af7d
Agustín Rodríguez <agurz@icloud.com>
2018-04-26 11:40:55 -0300

    tests moved to a private cloud

--- test/index.js
+++ test/index.js
@@ -1,15 +1,15 @@
 const Socket = require('socket.io-client');
 
-var socket = Socket('http://localhost:' + process.argv[2]);
+var socket = Socket('http://agurz.ddns.net:' + process.argv[2]);
 
 socket.emit('refresh', {
-    user: '5ae0e171677187162a948b8a',
+    user: '5ae1d52121da2b18b45d7435',
     latitude: -34.603722, // user lat
     longitude: -58.381592  // user lon
 });
 
 socket.emit('refresh', {
-    user: '5ae0e171677187162a948b8a',
+    user: '5ae1d52121da2b18b45d7435',
     latitude: -37.979858, // user lat
     longitude: -57.589794  // user lon
 });
