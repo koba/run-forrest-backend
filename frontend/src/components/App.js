f9e31aba8dbf666b69d38094aada85468537aad0
Agustín Rodríguez <agurz@icloud.com>
2018-04-26 19:51:23 -0300

    added run path to map

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,3 +1,4 @@
+import polyline from '@mapbox/polyline';
 import React, { Component } from 'react';
 import { Marker, Polyline } from 'react-google-maps';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
@@ -54,17 +55,48 @@ class App extends Component {
           loadingElement={<div style={{ height: `100%` }} />}
           mapElement={<div style={{ height: `100%` }} />}
           googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places">
-          {
-            Object.keys(this.state.runState).length && Object.keys(this.state.runState.runners).map(k => {
-              let last = this.state.runState.runners[k].coordinates.length - 1;
-              return (
-                <Marker
-                  key={k}
-                  position={{ lat: this.state.runState.runners[k].coordinates[last][1], lng: this.state.runState.runners[k].coordinates[last][0] }}  
+            {/** run's path **/}
+            {
+              this.state.runs.length && (
+                <Polyline
+                  key={'run'}
+                  path={polyline.decode(this.state.runs[0].path).map(decodedPolyline => ({ lat: decodedPolyline[0], lng: decodedPolyline[1]}))}  
+                  options={{
+                    geodesic: true,
+                    strokeColor: '#375b41',
+                    strokeWeight: 2
+                  }}
                 />
               )
-            })
-          }
+            }
+            {/** runner's markers **/}
+            {
+              Object.keys(this.state.runState).length && Object.keys(this.state.runState.runners).map(k => {
+                let last = this.state.runState.runners[k].coordinates.length - 1;
+                return (
+                  <Marker
+                    key={k}
+                    position={{ lat: this.state.runState.runners[k].coordinates[last][1], lng: this.state.runState.runners[k].coordinates[last][0] }}  
+                  />
+                )
+              })
+            }
+            {/** runner's polylines **/}
+            {
+              Object.keys(this.state.runState).length && Object.keys(this.state.runState.runners).map(k => {
+                return (
+                  <Polyline
+                    key={k}
+                    path={polyline.decode(this.state.runState.runners[k].route.geometry).map(decodedPolyline => ({ lat: decodedPolyline[0], lng: decodedPolyline[1]}))}  
+                    options={{
+                      geodesic: true,
+                      strokeColor: '#ff0000',
+                      strokeWeight: 2
+                    }}
+                  />
+                )
+              })
+            }
         </Map>
       </div>
     );
