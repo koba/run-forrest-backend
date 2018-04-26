53c8c2f16cae68c61b60273398b1badd94456567
Agustín Rodríguez <agurz@icloud.com>
2018-04-26 00:01:07 -0300

    lets try another approach..

--- scripts/download-osrm.sh
+++ scripts/download-osrm.sh
@@ -1,8 +1,8 @@
 if [ ! -d "data/osrm/argentina" ]
 then 
-    wget https://s3.amazonaws.com/run-forrest/argentina.tar.gz
-    pwd
-    ls
     mkdir -p data/osrm
-    tar zxvf argentina.tar.gz -C data/osrm
+    cd data/osrm
+    wget https://s3.amazonaws.com/run-forrest/argentina.tar.gz
+    tar zxvf argentina.tar.gz
+    cd ../..
 fi
