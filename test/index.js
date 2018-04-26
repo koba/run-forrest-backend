b2e6ffd0dee51d8d30e3cb8f42bb9f4ccee0966f
Agustín Rodríguez <agurz@icloud.com>
2018-04-26 08:37:42 -0300

    changed user used for tests

--- test/index.js
+++ test/index.js
@@ -1,15 +1,15 @@
 const Socket = require('socket.io-client');
 
-var socket = Socket('https://ec2-34-237-245-91.compute-1.amazonaws.com:' + process.argv[2]);
+var socket = Socket('http://ec2-34-237-245-91.compute-1.amazonaws.com:' + process.argv[2]);
 
 socket.emit('refresh', {
-    user: '5ae0e171677187162a948b8a',
+    user: '5ae1b9cc9c2259217435602a',
     latitude: -34.603722, // user lat
     longitude: -58.381592  // user lon
 });
 
 socket.emit('refresh', {
-    user: '5ae0e171677187162a948b8a',
+    user: '5ae1b9cc9c2259217435602a',
     latitude: -37.979858, // user lat
     longitude: -57.589794  // user lon
 });
