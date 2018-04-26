1acf01fde208387dfffddb64accfa1d6eb7031f9
Agustín Rodríguez <agurz@icloud.com>
2018-04-25 23:45:07 -0300

    create osrm data folder before extract downloaded tar

--- scripts/download-osrm.sh
+++ scripts/download-osrm.sh
@@ -1,5 +1,6 @@
 if [ ! -d "data/osrm/argentina" ]
 then 
     wget https://s3.amazonaws.com/run-forrest/argentina.tar.gz
+    mkdir -p data/osrm/argentina
     tar zxvf argentina.tar.gz -C data/osrm/argentina
 fi
