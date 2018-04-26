ad4a3820ba58f84edf702d740a870f6341b72621
Agustín Rodríguez <agurz@icloud.com>
2018-04-26 20:07:54 -0300

    change map default settings

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -3,8 +3,8 @@ import { withGoogleMap, withScriptjs, GoogleMap, Marker, Polyline } from 'react-
 
 export const Map = withScriptjs(withGoogleMap((props) => (
     <GoogleMap
-        defaultCenter={{ lat: -34.603722, lng: -58.381592 }}
-        defaultZoom={8}>
+        defaultCenter={{ lat: -34.574195, lng: -58.400127 }}
+        defaultZoom={14}>
             {props.children}
     </GoogleMap>
 )));
